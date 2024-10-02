9f08a47a8282b9c9f21c4643201c74079a1c6b2e
elnuage <qb.secu@gmail.com>
2024-10-02 21:14:09 +0200

    Nop

--- src/commands/help/ping.dart
+++ src/commands/help/ping.dart
@@ -7,7 +7,11 @@ import '../../components/button.dart';
 final class PingCommand implements CommandDeclaration {
   FutureOr<void> handle(ServerCommandContext ctx, {required int value}) {
     print(value);
-    ctx.interaction.reply(content: '$value', components: [RowBuilder()..addComponent(Button().testButton)]);
+    ctx.interaction.reply(content: '$value', components: [
+      RowBuilder()
+        ..addComponent(Button('primary1', 'test1', true).primary)
+        ..addComponent(Button('secondary1', 'Secondaire', false).secondary)
+    ]);
   }
 
   @override
