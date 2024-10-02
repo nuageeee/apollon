7637752b8eec2afa0278adc549300be9b2944c78
elnuage <qb.secu@gmail.com>
2024-10-02 21:14:02 +0200

    Update args buttom
    Add args to button to reuse it to the futur

--- src/components/button.dart
+++ src/components/button.dart
@@ -1,7 +1,17 @@
 import 'package:mineral/api.dart';
 
 class Button {
-  final testButton = ButtonBuilder.primary('p')
-    ..setLabel('Click mon s')
-    ..setDisabled(false);
+  String id, label;
+  bool isDisabled;
+  late final primary, secondary, success, danger;
+
+  Button(this.id, this.label, this.isDisabled) {
+    primary = ButtonBuilder.primary(id)
+      ..setLabel(label)
+      ..setDisabled(isDisabled);
+
+    secondary = ButtonBuilder.secondary(id)
+      ..setLabel(label)
+      ..setDisabled(isDisabled);
+  }
 }
